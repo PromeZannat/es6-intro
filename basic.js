a6b402d10a24bd5b13c42493766a747079c0d455
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2022-02-21 21:18:10 +0600

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,44 @@
+//variable;
+var deposit = 400;
+//consition
+if(deposit>500){
+
+}
+else if(deposit<500){
+
+}
+else if (deposit !=500){
+
+}
+else if(deposit>=500){
+
+}
+else{
+
+}
+//array
+const numbers = [45, 12, 34, 65, 345];
+const numberCount = numbers.length;
+numbers.push(111);
+numbers.pop();
+numbers[2] = 44;
+//check whether 222 included in the array
+if(numbers.indexOf(222) != -1){
+
+}
+if(numbers.includes(222)){
+
+}
+//loop
+//while, for
+for(const number of numbers){
+
+}
+//function
+function fullName(first, second){
+    const name = first + ' ' +second;
+}
+const person = fullName('Rahim', 'Ali');
+
+//object 
+const bottle = {color: 'yellow', contain: 'water', price: 50}; 
