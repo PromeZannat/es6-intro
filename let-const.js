bf579a28279d7062f5d719e3d1121e69f84a83a6
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2022-02-23 20:29:02 +0600

    Add files via upload

---
+++ let-const.js
@@ -0,0 +1,23 @@
+let balance = 1240;
+balance = 1340;
+
+const userName = 'janpakhi potas';
+const weTogether = 'ami ' + userName;
+console.log(weTogether);
+
+const numbers = [34, 23, 45, 70];
+//numbers = [99, 145, 2]; not allowed reasigned
+
+numbers.push(555);
+console.log(numbers);
+let sum =0;
+for(let i =0; i<numbers.length; i++){
+    const number = numbers[i];
+    console.log(number);
+    sum= sum+numbers;
+    
+}
+console.log(sum);
+
+const student = {roll: 101, name: 'mofajjol', job: 'intern'};
+studentname = 'mofij';
