5e2e063575ee9c8d3161a2a93aad54b5a4b9e704
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2022-02-23 15:47:33 +0600

    Add files via upload

---
+++ arrow-function.js
@@ -0,0 +1,14 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+//document.getElementById('my-btn').onclick= function handleEvent(){
+
+//};
+
+const sum = add(15, 12);
+console.log(sum);
+
+const add2 = (num1, num2) => num1 + num2;
+const sum1 = 
+console.log(add2);
