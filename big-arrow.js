ebec05d0d0806fe8b523ef9c21ba8c518e1e3eec
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2022-02-23 15:48:42 +0600

    Add files via upload

---
+++ big-arrow.js
@@ -0,0 +1,21 @@
+const add = (num1, num2) => num1 + num2;
+const sum = add(22, 55);
+console.log(sum);
+
+const multiply = (num1, num2, num3) => num1*num2*num3;
+const result = multiply(2,3,4);
+console.log(result);
+
+const tenTimes = (num) => num *10;
+const output = tenTimes(13);
+console.log(output);
+
+const doMath = (x,y) => {
+    const sum = x+y;
+    const dif = x-y;
+    const output = dif *5;
+    return output;
+};
+const total = doMath(12,5);
+console.log(total);
+
