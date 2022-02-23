a075b78d4a8a9411ae7efbefb228780967b10066
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2022-02-23 20:29:53 +0600

    Add files via upload

---
+++ template-string.js
@@ -0,0 +1,18 @@
+const multiple = `this is my first
+this is second 
+this is third
+this is four`;
+
+const count = 5;
+const oldHeader = '<h3 class = "friend-name">Friend</h3>'
+const new1 = `<h3 class = "friend-name">Friend-${count}</h3>` 
+
+//console.log(new1);node template-string.js
+
+const first = 'mamun';
+const last = 'chowdhury';
+const fullName = `Name is ${first} hasan ${last}`;
+
+console.log(fullName);
+
+//caret sign ` ` template string toiri kora jay.
